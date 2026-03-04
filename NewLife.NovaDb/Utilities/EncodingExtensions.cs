2dad29f25c84dfc4c42904c2c7ef455339dbd470
Allen Cai <caizz520@gmail.com>
2026-03-04 22:15:16 +0800

    重构字节数组池化管理，统一为PooledBytes
    删除PooledUtf8Bytes相关实现，新增PooledBytes结构体和EncodingExtensions扩展类，实现更通用的对象池字节数组管理与编码转换。

---
+++ NewLife.NovaDb/Utilities/EncodingExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Buffers;
+using System.Text;
+
+namespace NewLife.NovaDb.Utilities
+{
+    /// <summary>
+    /// 提供字符串与 UTF-8 编码字节数组之间的转换扩展方法，使用对象池管理字节数组以提高性能。
+    /// </summary>
+    internal static class EncodingExtensions
+    {
+        private static readonly Encoding Encoding = Encoding.UTF8;
+
+        /// <summary>
+        /// 将字符串转换为使用对象池管理的 UTF-8 编码字节数组。
+        /// </summary>
+        /// <param name="value">要转换的字符串。</param>
+        /// <returns>返回一个 <see cref="PooledBytes"/> 实例，包含 UTF-8 编码的字节数组。</returns>
+        public static PooledBytes ToPooledUtf8Bytes(this string value) => Encoding.GetPooledEncodedBytes(value);
+
+        /// <summary>
+        /// 将字符串转换为使用对象池管理的指定编码的字节数组。
+        /// </summary>
+        /// <param name="encoding">要使用的编码。</param>
+        /// <param name="value">要转换的字符串。</param>
+        /// <returns>返回一个 <see cref="PooledBytes"/> 实例，包含指定编码的字节数组。</returns>
+        public static PooledBytes GetPooledEncodedBytes(this Encoding encoding, string value)
+        {
+            var length = encoding.GetByteCount(value);
+            var pooledBytes = ArrayPool<byte>.Shared.Rent(length);
+            encoding.GetBytes(value, 0, value.Length, pooledBytes, 0);
+            return new PooledBytes(pooledBytes, length);
+        }
+    }
+}
